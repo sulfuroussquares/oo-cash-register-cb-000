c32068958ea8e6f27d9b017e0c7976cfdce5ba5d
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-05 03:56:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ def add_item(title, price, quantity = 1)
 @price = price
 if (quantity > 0)
   @total += (price*quantity)
-  @items << title
+    quantity.times {@items << title}
   #endif
 end
 #end add_item definition
