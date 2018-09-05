d899e81c3f5497fa0feb2c4ecaa75510e1f1db5d
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-05 03:42:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+attr_accessor :total, :discount
 
+def initialize(discount = 0)
+  #total is an instance variable and 0 by default. We won't initialize an instance with a value other than 0
+  @total = 0
+  @discount = discount
+end
+
+def add_item(title, price, quantity = 1)
+@title = title
+@quantity = quantity
+@price = price
+if (quantity > 0)
+  @total += (price*quantity)
+  #endif
+end
+#end add_item definition
+end
+
+def apply_discount
+  if @discount != 0
+    disc_amount = @discount/100
+    disc_subtotal = disc_amount*@total
+    @total -= disc_subtotal
+  end
+end
+
+end
