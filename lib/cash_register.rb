2e2c9b54523a32d31f6becf6af7ae8f6d7928a7d
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-09-05 03:43:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ def apply_discount
   if @discount != 0
     disc_amount = @discount/100
     disc_subtotal = disc_amount*@total
-    @total -= disc_subtotal
+    @total = @total - disc_subtotal
   end
   @total
 end
